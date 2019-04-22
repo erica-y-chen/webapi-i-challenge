e14b2cb1904418a66991171385ad97f51261e899
erica-y-chen <eyufanchen@gmail.com>
2019-04-22 14:10:12 -0700

    add and post requests

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // implement your API here
+const express = require('express');
+
+//cpackage implemented locally
+const db = require('./data/db.js');
+
+const server = express(); 
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send("It's alive!");
+  });
+  
+
+server.get('/users', (req, res) => {
+db
+    .find()
+    .then(users => {
+    res.status(200).json(users);
+    })
+    .catch(err => {
+    // handle error
+    res.json({ error: err, message: 'Something broke' });
+    });
+});
+
+
+server.post('/users', (req,res) => {
+    //axios.post(url,data)=> data shows up as body on the server
+    const userInformation = req.body;
+    console.log('request body: ', userInformation)
+
+    db.insert(userInformation)
+    .then(user => {
+        res.status(201).json(user)
+    }).catch(err => {
+        // handle error
+        res.json({ error: err, message: 'error adding user' });
+        });
+});
+
+
+server.listen(5000, () => {
+    console.log('\n*** API running on port 5k ***\n');
+  });
