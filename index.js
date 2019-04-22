baf9f9ecafd61aceb0ff452e78c7ecc26d1d7628
erica-y-chen <eyufanchen@gmail.com>
2019-04-22 14:18:20 -0700

    delete function

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ db
     });
 });
 
-
+//add new user 
 server.post('/users', (req,res) => {
     //axios.post(url,data)=> data shows up as body on the server
     const userInformation = req.body;
@@ -33,13 +33,30 @@ server.post('/users', (req,res) => {
 
     db.insert(userInformation)
     .then(user => {
-        res.status(201).json(user)
+        res.status(201).json(user) 
     }).catch(err => {
         // handle error
         res.json({ error: err, message: 'error adding user' });
         });
 });
 
+// //get specific user by id 
+// server.get('/users/:id')
+//     db.findById(user => {
+//         res.status(201).json(user)
+//     })
+
+//delete users 
+
+server.delete('/users/:id', (req, res) => {
+    const userID = req.params.id; 
+    db.remove(userID).then(deleted => {
+        res.status(204).end(); //sends back response to the client saying successful
+    }) .catch(err => {
+        //handle error
+        res.status(500).json({error: err, message: 'error deleting the user'});
+    });
+});
 
 server.listen(5000, () => {
     console.log('\n*** API running on port 5k ***\n');
